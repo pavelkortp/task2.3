b5058d000d2eeff36c08a846a092f4143665517a
Pavlo <pavelkortp@gmail.com>
2023-10-27 23:14:54 +0300

    adds udp client and server

--- tcp_client.ts
+++ tcp_client.ts
@@ -3,14 +3,14 @@ var net = require('net');
 var client = new net.Socket();
 client.connect(1337, '127.0.0.1', function() {
 	console.log('Connected');
-	client.write('Hello, from Pavlo!!!');
+	client.write('Hello, server!!!');
 });
 
 client.on('data', function(data:string) {
 	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
+	// client.destroy(); // kill client after server's response
 });
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+// client.on('close', function() {
+// 	console.log('Connection closed');
+// });
--- tcp_server.ts
+++ tcp_server.ts
@@ -1,6 +1,7 @@
 var net = require('net');
 
 var server = net.createServer(function (socket: any) {
+    
     socket.write('Hello from server\r\n');
     socket.pipe(socket);
 });
